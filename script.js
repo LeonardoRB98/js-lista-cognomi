4df61334e894a3dedbd5de94dc63f91b5f6209e9
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-16 18:14:56 +0100

    esercizio completo

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+
 // 1.Chiedere all’utente il cognome,
-// 2.inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
-// 3.Stampa la lista ordinata alfabeticamente.
-// 4.Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
-
-
-
 var cognomeUtente = prompt('inserisci il tuo cognome');
 console.log(cognomeUtente);
 
+// 2.inserirlo in un array con altri cognomi
 var listaCognomi = ['Pesce', 'Castenetto', 'Passaro', 'Grimaldi', 'Rossi', 'Verdi', 'Bianchi'];
 listaCognomi.push(cognomeUtente);
 console.log(listaCognomi);
+
+// 3.Stampa la lista ordinata alfabeticamente.
 console.log(listaCognomi.sort());
+
+// 4.Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
+
 for (var i = 0; i < listaCognomi.length; i++) {
-document.getElementById('lista').innerHTML += "<li>" + listaCognomi[i] + "</li><br>";
+  document.getElementById('lista').innerHTML += "<li>" + listaCognomi[i] + "</li><br>";
+  if (cognomeUtente == listaCognomi[i]) {
+  console.log(i + 1);
+  document.getElementById('mia_posizione').innerHTML = i + 1;
+  }
 }
